fb73580065c2d5e96accf6f7bf5c6a129c725b8a
Tom X Nguyen <tom81094@gmail.com>
2023-09-10 19:05:33 +0700

    chore: make it more readable

--- next/src/hooks/useModels.ts
+++ next/src/hooks/useModels.ts
@@ -22,11 +22,29 @@ const mapModels = (...names: string[]): LLMModel[] => names.map((name) => ({
 export const llmHosts: LLMHost[] = [
   {
     url: "https://openrouter.ai/api/v1",
-    models: mapModels(CLAUDE2, CODE_LLAMA2_34B, LLAMA2_13B, LLAMA2_70B, GPT_35_TURBO, GPT_35_TURBO_16K, GPT_4, GPT_4_32K, PALM2, PALM2_CODE, HERMES_LLAMA2_13B, AIROBOROS_70B)
+    models: mapModels(
+      CLAUDE2,
+      CODE_LLAMA2_34B,
+      LLAMA2_13B,
+      LLAMA2_70B,
+      GPT_35_TURBO,
+      GPT_35_TURBO_16K,
+      GPT_4,
+      GPT_4_32K,
+      PALM2,
+      PALM2_CODE,
+      HERMES_LLAMA2_13B,
+      AIROBOROS_70B
+    )
   },
   {
     url: "https://api.endpoints.anyscale.com/v1",
-    models: mapModels(LLAMA2_7B_AS, LLAMA2_13B_AS, LLAMA2_70B_AS, CODE_LLAMA2_34B_AS)
+    models: mapModels(
+      LLAMA2_7B_AS,
+      LLAMA2_13B_AS,
+      LLAMA2_70B_AS,
+      CODE_LLAMA2_34B_AS
+    )
   },
 ]
 
